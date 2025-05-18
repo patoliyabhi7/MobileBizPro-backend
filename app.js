f650603143eb4648e2de35e154891ceb4c1470c7
patoliyabhi7 <patoliyabhi7@gmail.com>
2025-05-18 19:58:15 +0530

    cors setup

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes); 
+app.use('/api/users', userRoutes);
 
 module.exports = app;
