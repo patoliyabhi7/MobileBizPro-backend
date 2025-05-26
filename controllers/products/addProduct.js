6790e081f9d62c613f76ceeec32255a3c747df30
yakshbhesaniya <yakshbhesaniya@gmail.com>
2025-05-26 11:09:42 +0530

    Product SKU Autogenerate feature added

--- controllers/products/addProduct.js
+++ controllers/products/addProduct.js
@@ -2,6 +2,11 @@ const Product = require('../../models/productModel');
 
 exports.addProduct = async (req, res) => {
   try {
+    if (!req.body.sku) {
+        const lastProduct = await Product.findOne().sort({ _id: -1 });
+        const newSkuNumber = lastProduct ? parseInt(lastProduct.sku.replace('PROD', '')) + 1 : 1;
+        req.body.sku = `PROD${newSkuNumber.toString().padStart(4, '0')}`;
+    }
     const product = await Product.create(req.body);
     res.status(201).json({ message: 'Product created successfully', product });
   } catch (err) {
