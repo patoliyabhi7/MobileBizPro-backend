19ee7cc155387373d788d15e0b46aac74b5f6ce7
yakshbhesaniya <yakshbhesaniya@gmail.com>
2025-05-31 14:16:25 +0530

    invoiceLayout api added

--- app.js
+++ app.js
@@ -62,4 +62,7 @@ app.use('/api/account-types', accountTypeRoutes);
 const accountRoutes = require('./routes/accountRoutes');
 app.use('/api/accounts', accountRoutes);
 
+const invoiceRoutes = require('./routes/invoiceRoutes');
+app.use('/api/invoices', invoiceRoutes);
+
 module.exports = app;
